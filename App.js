4b68054081a282227fc0796881db0d90607fcd2e
MasihBelajar212 <bagusmade212@gmail.com>
2022-12-22 00:50:54 +0700

    coba commit dulu

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Contoh Text Doang DLu</Text>
+      <Text>Gk ada Masalah Harusnya</Text>
+    </>
+  );
+};
+
+export default App;
